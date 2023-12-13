b496d5c182470b779873ba273d02b9c2f3c3a253
atsutama2 <atsutama.0713@gmail.com>
2023-12-13 15:20:09 +0900

    Update main.go

--- main.go
+++ main.go
@@ -10,11 +10,4 @@ func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println("Hogehoge")
 	fmt.Println("fugafuga")
-	fmt.Println("test1")
-	fmt.Println("test2")
-	fmt.Println("test3")
-	fmt.Println("test4")
-	fmt.Println("test5")
-	fmt.Println("test6")
-	fmt.Println("test7")
 }
